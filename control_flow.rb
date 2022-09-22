2e1aaf202845dac1ca43f5d28549376bc17ca458
Nicolas Telep <ndtelep@gmail.com>
2022-09-22 11:11:13 -0600

    commit tree e400b1005c2da0c16f91370aa6fcc07244bd5a63

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,21 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+ else
+    "Access denied"
+ end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature < 65
+    "It's a little chilly out there!"
+  elsif temperature >= 65 && temperature <= 85
+    "It's perfect out there!"
+  else
+    "It's too dang hot out there!"
+  end
 end
 
 def fizzbuzz(num)
